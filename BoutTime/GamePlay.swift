0933c6f5197a8b3a01eb87c5f4e7e5412ed1a687
Eric Romanik <eric@id8.com>
2018-02-07 12:03:44 -0800

    Basic timer working

---
+++ BoutTime/GamePlay.swift
@@ -0,0 +1,14 @@
+//
+//  GamePlay.swift
+//  BoutTime
+//
+//  Created by Eric Romanik on 2/7/18.
+//  Copyright © 2018 Romanik. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var seconds: Int = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        runTimer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1     // decrement seconds
+        let formattedSeconds = String(format:"%02i", seconds) // format seconds to include leading zero
+        timerLabel.text = "0:\(formattedSeconds)" // update the label
+    }
+    
+    
 }
 
